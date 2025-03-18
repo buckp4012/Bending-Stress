8c8aa95e1eb73f902f02bd1275b7ac3b6ff2960e
buckp4012 <127550556+buckp4012@users.noreply.github.com>
2025-03-18 09:11:11 -0500

    Update Bending_Stress.py

--- Bending_Stress.py
+++ Bending_Stress.py
@@ -2318,14 +2318,14 @@ def handle_selections(motor_equipment, motor1, motor2, seal_equipment, seal1, se
                 go.Scatter(
                     x=MyArray_ft_increment,
                     y=MyArray_stress,
-                    mode="lines+markers",
+                    mode="lines",
                     name="Stress",
                     line=dict(color="red")
                 ),
                 go.Scatter(
                     x=MyArray_ft_increment,
                     y=MyArray_deflection,
-                    mode="lines+markers",
+                    mode="lines",
                     name="Deflection",
                     line=dict(color="blue"),
                     yaxis="y2"  # Specify that this trace should use the second y-axis
